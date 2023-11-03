170feb1417e105b652fa5bad3a899facc00fea59
mallvielfrass <jack@bykollab.com>
2023-11-03 18:36:01 +0300

    add back api address

--- vite.config.js
+++ vite.config.js
@@ -40,5 +40,12 @@ export default defineConfig({
   },
   server: {
     port: 3100,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });

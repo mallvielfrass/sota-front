224523385098771fc5d9ea1b61f234e0bee0b16b
mallvielfrass <jack@bykollab.com>
2023-11-14 19:33:38 +0300

    fix host listen

--- vite.config.js
+++ vite.config.js
@@ -39,6 +39,7 @@ export default defineConfig({
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
   server: {
+    host: "0.0.0.0",
     port: 3100,
     proxy: {
       "/api": {
